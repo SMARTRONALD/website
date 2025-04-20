6248ec27aa676bcccf1bd57bfbaeb52f54b3cb48
Smart Ronald <smartronald86@gmail.com>
2025-04-20 10:22:35 +0300

    Fixing port issues for Render Deployment

--- app.py
+++ app.py
@@ -107,4 +107,6 @@ def education():
     return render_template('education.html', education=education,email=email)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
